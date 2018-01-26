7754f3b49497d17aabcebba59aedbaa438ade8c7
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-01-26 18:11:49 -0300

    update tests

--- src/modules/search/components/__tests__/HotelList.spec.js
+++ src/modules/search/components/__tests__/HotelList.spec.js
@@ -2,6 +2,21 @@ import React from "react";
 import { shallow } from "enzyme";
 import { HotelList } from "../HotelList";
 
+const hotels = [
+	{
+		id: 1,
+		name: "Hotel",
+		stars: 4,
+		amenities: [],
+		image: "",
+		price: 100.0
+	}
+];
 it("renders without crashing", () => {
 	const component = shallow(<HotelList hotels={[]} />);
+});
+
+it("renders the proper number of cards", () => {
+	const component = shallow(<HotelList hotels={hotels} />);
+	expect(component.find("HotelCard").length).toBe(1);
 });
